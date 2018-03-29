df012c5d0295893a25e9154c57906373c6fac864
Jascha Ehrenreich <jascha@jaeh.at>
2018-03-29 20:45:28 +0200

    cleanup render

--- src/render.js
+++ src/render.js
@@ -6,17 +6,16 @@ import { withRender } from "@hyperapp/render"
 
 const fp = path.join(process.cwd(), "src", "client", "public", "index.html")
 const html = fs.readFileSync(fp).toString()
-console.log({ html })
+const [head, footer] = html.split("<div>Loading...</div>")
 
 export const render = props => (req, res) => {
-  console.log("start render")
-  const { client } = props
   res.type("text/html")
-  const [head, footer] = html.split("<div>Loading...</div>")
   res.write(head)
+
+  const { client } = props
   const main = withRender(app)(client.state, client.actions, client.view)
   const stream = main.toStream()
-  console.log({ main, s: main.toString() })
+
   stream.pipe(res, { end: false })
   stream.on("end", () => {
     res.write(footer)
