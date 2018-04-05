63cee1db6ceee4fc5f0c50535f37909541bf6ac7
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-05 18:08:22 +0200

    make setting client.state.location.pathname save

--- src/render.js
+++ src/render.js
@@ -4,15 +4,22 @@ import path from "path"
 import { h, app } from "hyperapp"
 import { withRender } from "@hyperapp/render"
 
-const fp = path.join(process.cwd(), "src", "client", "public", "index.html")
+const fp = path.join(process.cwd(), "src", "client", "index.html")
 const html = fs.readFileSync(fp).toString()
-const [head, footer] = html.split("<div>Loading...</div>")
+const splitPoint = "<body>"
+const [head, footer] = html.split(splitPoint)
 
 export const render = props => (req, res) => {
   res.type("text/html")
-  res.write(head)
+  res.write(head + splitPoint)
 
   const { client } = props
+
+  // make the router render the correct view
+  client.state.location = {
+    pathname: req.path
+  }
+
   const main = withRender(app)(client.state, client.actions, client.view)
   const stream = main.toStream()
 
